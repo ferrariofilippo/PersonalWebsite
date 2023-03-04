b1c9fcab71443a48420d6c0630c789f38eaf09e0
ferrariofilippo <f.filippo49@gmail.com>
2023-03-04 18:56:35 +0100

    Footer Update & Credits

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,55 +13,114 @@ export default function Footer() {
             className="d-flex flex-wrap justify-content-between align-items-center border-top py-3 mt-3 border-top"
           >
             <div
-              className="col-sm-8 col-12 d-flex align-items-center flex-sm-row flex-column mx-sm-0 mx-auto"
+              className="col-sm-8 col-12 d-flex flex-column mb-auto"
             >
-              <a
-                className="text-muted text-decoration-none"
-                href="/"
+              <div
+                className="d-flex align-items-center flex-sm-row flex-column mx-sm-0 mx-auto"
               >
-                <Image
-                  alt="Ferrario Filippo"
-                  src="/ferrariofilippo.png"
-                  height="32"
-                  width="32"
-                  className="rounded-4 mx-3"
-                />
-              </a>
-              <span
-                className="tetx-muted"
+                <a
+                  className="text-decoration-none"
+                  href="/"
+                >
+                  <Image
+                    alt="Ferrario Filippo"
+                    src="/ferrariofilippo.png"
+                    height="32"
+                    width="32"
+                    className="rounded-4 mx-3"
+                  />
+                </a>
+                <span
+                  className="text-muted"
+                >
+                  © 2023 Filippo Ferrario
+                </span>
+              </div>
+              <div
+                className="mx-3 mt-2 d-flex flex-row justify-content-sm-start justify-content-center"
               >
-                © 2023 Filippo Ferrario
-              </span>
+                <a
+                  className="custom-link font-small text-muted"
+                  href="https://github.com/ferrariofilippo/PersonalWebsite"
+                >
+                  Code
+                </a>
+                <span
+                  className="mx-3 text-muted"
+                >
+                  |
+                </span>
+                <a
+                  className="custom-link font-small text-muted"
+                  href="/privacy"
+                >
+                  Privacy
+                </a>
+              </div>
             </div>
 
-            <ul
-              className="nav col-sm-4 col-12 justify-content-sm-end justify-content-center list-untyled d-flex mx-sm-0 mx-auto mt-sm-0 mt-3">
-              <li>
+            <div
+              className="col-sm-4 col-12 d-flex flex-column"
+            >
+              <ul
+                className="nav  justify-content-sm-end justify-content-center list-untyled d-flex mx-sm-0 mx-auto mt-sm-0 mt-3"
+              >
+                <li>
+                  <a
+                    href="https://github.com/ferrariofilippo"
+                    className="custom-link text-decoration-none mx-3"
+                    style={{ "--color": "#018574", "fontWeight": 600 }}
+                  >
+                    GH
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/ferrariofilippo"
+                    className="custom-link text-decoration-none"
+                  >
+                    IN
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/fferrario4"
+                    className="custom-link text-decoration-none mx-3"
+                  >
+                    TW
+                  </a>
+                </li>
+              </ul>
+
+              <div
+                className="d-flex flex-column text-sm-end text-center mx-3"
+              >
+                <h5
+                  className="mt-3 mb-0"
+                >
+                  Credits
+                </h5>
                 <a
-                  href="https://github.com/ferrariofilippo"
-                  className="custom-link text-decoration-none mx-3"
+                  className="custom-link font-small"
+                  href="https://vercel.com/"
                   style={{ "--color": "#018574", "fontWeight": 600 }}
                 >
-                  GH
+                  Powered by Vercel
                 </a>
-              </li>
-              <li>
                 <a
-                  href="https://www.linkedin.com/in/ferrariofilippo"
-                  className="custom-link text-decoration-none"
+                  className="custom-link font-small text-muted"
+                  href="https://getbootstrap.com/"
                 >
-                  IN
+                  Bootstrap
                 </a>
-              </li>
-              <li>
                 <a
-                  href="https://twitter.com/fferrario4"
-                  className="custom-link text-decoration-none mx-3"
+                  className="custom-link font-small text-muted"
+                  href="https://fonts.google.com/"
                 >
-                  TW
+                  Google Fonts
                 </a>
-              </li>
-            </ul>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
