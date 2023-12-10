251ea9f3d3285763e353807bedebc74f820a8f89
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2023-12-10 13:31:52 +0100

    Add SpeedInsights

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import Footer from './footer';
 import { AnalyticsWrapper } from './analytics';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Layout({ children }) {
   return (
@@ -10,6 +11,7 @@ export default function Layout({ children }) {
         {children}
         <Footer />
         <AnalyticsWrapper />
+        <SpeedInsights />
       </div>
     </>
   )
